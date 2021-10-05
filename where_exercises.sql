57c78081336d967ecf1616e4e6f3973145c809fc
afonse03 <amanda.m.fonseca03@gmail.com>
2021-10-05 09:05:21 -0600

    finished with where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -39,4 +39,10 @@ WHERE (first_name = 'Irena' OR
 SELECT * FROM employees
 WHERE last_name LIKE '%E' OR last_name LIKE 'E%';
 
+SELECT * FROM employees
+WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
 
